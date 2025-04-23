3327e3aae45dcd772f8140dbcd0330a109d3d5cb
Nikita Dhiman <nikitadhiman@Nikitas-MacBook-Air.local>
2025-04-23 18:31:22 +0530

    Redux started

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import ContactUs from './components/ContactUs';
 import ErrorComponent from './components/ErrorComponent';
 import RestaurantMenu from './components/RestaurantMenu';
 import UserContext from './utils/contexts/UserContext';
+import { Provider } from 'react-redux';
+import appStore from './utils/store/appStore';
 // import Grocery from './components/Grocery';
 
 // Lazy import
@@ -25,12 +27,14 @@ const AppLayout = () => {
   }, []);
 
   return (
-    <UserContext.Provider value={{ loggedInUser: userName, setUserName }}>
-      <div id="app">
-        <Header />
-        <Outlet />
-      </div>
-    </UserContext.Provider>
+    <Provider store={appStore}>
+      <UserContext.Provider value={{ loggedInUser: userName, setUserName }}>
+        <div id="app">
+          <Header />
+          <Outlet />
+        </div>
+      </UserContext.Provider>
+    </Provider>
   );
 };
 
